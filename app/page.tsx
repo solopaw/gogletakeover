586335e8d0b3ad42fe2a57d7432a2ac66830dede
solopaw <adamjoelfraser@icloud.com>
2025-04-04 16:00:45 +1000

    modified:   .gitignore 	modified:   app/page.tsx 	new file:   components/Button.tsx 	new file:   lib/utils.ts 	new file:   next-env.d.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   styles/globals.css 	modified:   tsconfig.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import {Button} from "@/components/Button";
+import { CircleUser, Globe, HomeIcon, Mail } from "lucide-react";
+
 export default function HomePage() {
+  let animations = 'hover:scale-150';
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <h1>Welcome to My Next.js App</h1>
       <p>This is the home page.</p>
-      <div className="animate-in fade-in zoom-in bg-blue-100 ">...</div>
-
+      <div className="animate-in spin-in duration-100 bg-blue-100 h-20 w-20 text-center content-center">...</div>
+      <Button className={"bg-sky-500"+" "+animations}><HomeIcon /></Button>
+<Button className={"bg-rose-500"+" "+animations}><CircleUser/></Button>
+<Button className={"bg-emerald-500"+" "+animations}><Mail/></Button>
+<Button className={"bg-violet-500"+" "+animations}><Globe/></Button>
     </main>
   );
 }
